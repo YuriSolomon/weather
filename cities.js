2acdff75edc05b5590c6e933ca0e192c8ad0237c
YuriSolomon <juristein21@gmail.com>
2018-09-14 17:17:44 +0200

    separate cities by weather

--- cities.js
+++ cities.js
@@ -2,7 +2,7 @@ Vue.component('cities', {
     props: ['cities_in'],
     template: `
     <div class="d-flex flex-row flex-wrap justify-content-around bg-secondary" id="newBook">
-        <div v-for="city in this.cities_in" class="square container">
+        <div v-for="city in this.cities_in">
             <city :city_info="city"></city>
         </div>
     </div>
--- city.js
+++ city.js
@@ -1,12 +1,38 @@
 Vue.component('city', {
     props: ['city_info'],
+    data: function () {
+        return {
+            isSun: this.city_info.weather[0].description.includes('sun'),
+            isRain: this.city_info.weather[0].description.includes('rain'),
+            isCloud: this.city_info.weather[0].description.includes('cloud')
+        }
+    },
     template: `
-    <div class="justify-content-center">
-        <div class="info">
-            {{this.city_info.name}}
-            {{this.city_info.main.temp}}
-            {{this.city_info.weather[0].description}}
+    <div class="city">
+        <div v-if="isSun === true">
+            <div class="info container square sun">
+                <h3>{{this.city_info.name}}</h3>
+                <h5>{{this.city_info.weather[0].description}}</h5>
+                <h3>{{this.city_info.main.temp}}ºC</h3>
+            </div>
+        </div>
+        <div v-else-if="isRain === true">
+            <div class="info container square rain">
+                <h3>{{this.city_info.name}}</h3>
+                <h5>{{this.city_info.weather[0].description}}</h5>
+                <h3>{{this.city_info.main.temp}}ºC</h3>
+            </div>
+        </div>
+        <div v-else-if="isCloud === true">
+            <div class="info container square cloud">
+                <h3>{{this.city_info.name}}</h3>
+                <h5>{{this.city_info.weather[0].description}}</h5>
+                <h3>{{this.city_info.main.temp}}ºC</h3>
+            </div>
         </div>
     </div>
     `
 })
+
+
+// "{{this.city_info.weather[0].main}}.includes('Sun')"
